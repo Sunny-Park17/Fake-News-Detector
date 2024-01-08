48926c4a182d22b6b990971eac0df9e536bca842
Sunny Park <sunnypark3428@gmail.com>
2024-01-07 21:21:48 -0800

    finished clean text function

--- main.py
+++ main.py
@@ -36,3 +36,10 @@ data = data_merge.drop(['title','subject','date'], axis = 1)
 def wordopt(text):
     text = text.lower()
     text = re.sub('\[,*?\]', '', text)
+    text = re.sub('\\W', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w\d\w*', '', text)
+    return text
