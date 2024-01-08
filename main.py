b0191f977fb4b8292fba39520b6c510dcbc03986
Sunny Park <sunnypark3428@gmail.com>
2024-01-07 21:36:35 -0800

    bug fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report
 import re
 import string
 
-#pulling data from csv files
+# pulling data from csv files
 data_fake = pd.read_csv('Fake.csv')
 data_true = pd.read_csv('True.csv')
 data_fake.head()
@@ -21,17 +21,17 @@ data_fake.shape, data_true.shape
 print(data_fake.shape, data_true.shape)
 
 dataF_manual_testing = data_fake.tail(10)
-for i in range(23480,23470,-1):
-    data_fake.drop([i], axis = 0, inplace = True)
+for i in range(23480, 23470, -1):
+    data_fake.drop([i], axis=0, inplace=True)
 
 dataT_manual_testing = data_true.tail(10)
 for i in range(21416, 21406, -1):
-    data_true.drop([i], axis = 0, inplace = True)
+    data_true.drop([i], axis=0, inplace=True)
 
-data_merge = pd.concat([data_fake, data_true], axis = 0)
+data_merge = pd.concat([data_fake, data_true], axis=0)
 data_merge.head(10)
 
-data = data_merge.drop(['title','subject','date'], axis = 1)
+data = data_merge.drop(['title', 'subject', 'date'], axis=1)
 
 def wordopt(text):
     text = text.lower()
@@ -43,3 +43,50 @@ def wordopt(text):
     text = re.sub('\n', '', text)
     text = re.sub('\w\d\w*', '', text)
     return text
+
+
+data['text'] = data['text'].apply(wordopt)
+
+x = data['text']
+y = data['class']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorization = TfidfVectorizer()
+xv_train = vectorization.fit_transform(x_train)
+xv_test = vectorization.transform(x_test)
+
+from sklearn.linear_model import LogisticRegression
+
+LR = LogisticRegression()
+LR.fit(xv_train, y_train)
+
+pred_lr = LR.predict(xv_test)
+LR.score(xv_test, y_test)
+print(classification_report(y_test, pred_lr))
+
+from sklearn.tree import DecisionTreeClassifier
+DT = DecisionTreeClassifier()
+DT.fit(xv_train, y_train)
+
+pred_dt = DT.predict(xv_test)
+DT.score(xv_test, y_test)
+print(classification_report(y_test, pred_dt))
+
+def output_lable(n):
+    if n == 0:
+        return "Fake News"
+    elif n == 1:
+        return "Not Fake News"
+def manual_testing(news):
+    testing_news = {"text":[news]}
+    new_def_test = pd.DataFrame(testing_news)
+    new_def_test['text'] = new_def_test['text'].apply(wordopt)
+    new_x_test = new_def_test['text']
+    new_xv_test = vectorization.transform(new_x_test)
+    pred_LR = LR.predict(new_xv_test)
+    pred_DT = DT.predict(new_xv_test)
+
+    return print("\n\nLR Prediction: {} \nDT Prediction: ".format(output_lable(pred_LR[0]), output_lable(pred_DT[0])))
